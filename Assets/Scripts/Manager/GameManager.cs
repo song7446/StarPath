466ce1599db61b583c5c289c18fad5a8971ad0b5
WonSeokSong <songwon4529@naver.com>
2025-12-08 22:30:58 +0900

    [Add] 게임 매니저 추가

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,17 @@
+using SongLib.Core.Singleton;
+using UnityEngine;
+
+public class GameManager : MonoBehaviourSingleton<GameManager>
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
