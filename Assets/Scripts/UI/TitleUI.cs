8c5b2de6c609c15a0cbfab5669fea5e2a209aab4
WonSeokSong <songwon4529@naver.com>
2025-12-09 14:46:49 +0900

    [Feat] exit 버튼 추가

--- Assets/Scripts/UI/TitleUI.cs
+++ Assets/Scripts/UI/TitleUI.cs
@@ -6,14 +6,25 @@ using UnityEngine.UI;
 public class TitleUI : MonoBehaviour
 {
     [SerializeField] private Button _startButton;
+    [SerializeField] private Button _exitButton;
 
     private void Awake()
     {
         _startButton.onClick.AddListener(OnClickStartButton);
+        _exitButton.onClick.AddListener(OnClickExitButton);
     }
 
     private void OnClickStartButton()
     {
         SceneLoader.Instance.LoadScene(SceneName.GameSceneName);
+    }
+
+    private void OnClickExitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
