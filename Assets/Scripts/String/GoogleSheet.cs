4e053324ec75b3d7d8463b415c91f2a99af22ca3
WonSeokSong <songwon4529@naver.com>
2025-12-11 18:54:28 +0900

    Revert "[Wip] 컷씬 대사 전환 진행중"
    This reverts commit ee2c9b886af9db4efb20b0ef448cef78bb51ab15.

--- Assets/Scripts/String/GoogleSheet.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public static class GoogleSheet
-{
-    public static string GoogleSheetURL =
-        "https://docs.google.com/spreadsheets/d/16D3Cy2z48fSpxbv1bQGsOW-v4VyzK4wqbWeS0Rlog7E/export?format=csv";
-}
