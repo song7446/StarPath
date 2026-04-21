a7bd4aa02b0de93df3f441a265fc06bdc6347379
WonSeokSong <songwon4529@naver.com>
2026-04-21 16:12:51 +0900

    [Refactor] 다음챕터 들어가기 리팩토링

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -13,40 +13,34 @@ public class GameManager : MonoBehaviourSingleton<GameManager>
 
     public async Task EnterNextChapter()
     {
-        if (!isFront) CurrentChapterId++;
-        isFront = !isFront;
-        
-        ChapterDataRepository.Instance.SetCurrentChapterAddress(CurrentChapterId, isFront);
-
-        await ChapterDataRepository.Instance.LoadCurrentChapterSO();
-
-
-        // 챕터 앞 -> 뒤
-        if (!isFront)
+        if (isFront) // 현재 앞부분(true)에서 뒷부분(false)으로 넘어갈 예정이라면
         {
             await StarSpawnManager.Instance.FadeOutAllStars();
-
-            _bootstrapper = new Bootstrapper();
-
-            Debug.Log("부트스트래퍼 시작");
-
-            _bootstrapper
-                .Add(DialogueDataRepository.Instance)
-                .Add(CutSceneManager.Instance)
-                .Run(() => GameStateMachine.Instance.ChangeState<CutSceneState>());
         }
-        // 챕터 뒤 -> 다음 챕터 앞
-        else
+
+        // 2. [데이터 페이즈] 챕터 ID 및 상태 갱신
+        if (!isFront) CurrentChapterId++;
+        isFront = !isFront;
+    
+        ChapterDataRepository.Instance.SetCurrentChapterAddress(CurrentChapterId, isFront);
+        await ChapterDataRepository.Instance.LoadCurrentChapterSO();
+
+        // 3. [초기화 페이즈] 부트스트래퍼 공통 세팅
+        _bootstrapper = new Bootstrapper();
+        Debug.Log($"부트스트래퍼 시작 (Chapter: {CurrentChapterId}, isFront: {isFront})");
+
+        // 공통 매니저 (항상 들어감)
+        _bootstrapper
+            .Add(DialogueDataRepository.Instance)
+            .Add(CutSceneManager.Instance);
+
+        // 챕터 앞부분일 때만 추가되는 매니저
+        if (isFront)
         {
-            _bootstrapper = new Bootstrapper();
-
-            Debug.Log("부트스트래퍼 시작");
-
-            _bootstrapper
-                .Add(DialogueDataRepository.Instance)
-                .Add(CutSceneManager.Instance)
-                .Add(StarSpawnManager.Instance)
-                .Run(() => GameStateMachine.Instance.ChangeState<CutSceneState>());
+            _bootstrapper.Add(StarSpawnManager.Instance);
         }
+
+        // 4. [실행 페이즈] 모두 세팅되었으면 Run!
+        _bootstrapper.Run(() => GameStateMachine.Instance.ChangeState<CutSceneState>());
     }
 }
